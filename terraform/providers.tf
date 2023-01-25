334d7e858d3f5e0d27d9f3f938c567789ed070a0
nok911 <niall.okeeffe@weareams.com>
2023-01-25 16:31:15 +0000

    terraform & ansible

---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+    shared_credentials_files = ["~/.aws/credentials"]
+    region = "eu-west-1"
+}
